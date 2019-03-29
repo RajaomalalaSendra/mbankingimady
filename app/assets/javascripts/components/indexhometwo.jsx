a44e5ea7e28b771be6f8d75a55f056febb8268a8
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-03-29 19:17:23 +0300

    the login and the signout has been created with bootstrap

--- app/assets/javascripts/components/indexhometwo.jsx
+++ app/assets/javascripts/components/indexhometwo.jsx
@@ -1,11 +1,20 @@
 const IndexHomeTwo = (props) => {
 	return(
 		<div>
-			<div style={{marginLeft: '1200px'}}>
-				<a href="/users/sign_in">sign in</a>||
-				<a href="/users/sign_up">sign up</a>
-			</div>
-			<h1>The Home Two is Here</h1>
+			<nav className="navbar navbar-default" style={{marginTop: "-50px",}}>
+			    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul className="nav navbar-nav navbar-left">
+				    	<li><a className="menu active" href="#home" >Home MbankingImady</a></li>
+				    </ul>
+			      	<ul className="nav navbar-nav navbar-right">
+			        	<li><a className="menu" href="#team">our team</a></li>
+			        	<li><a className="menu" href="#contact"> contact us</a></li>
+			        	<li><a className="menu" href="/users/sign_in">sign in ||</a></li>
+						<li><a className="menu" href="/users/sign_up">sign up</a></li>
+			      	</ul>
+			    </div>
+			</nav>
+			<h1 style={{marginLeft: '100px'}}>The Home Two is Here</h1>
 		</div>
 	)
 }
